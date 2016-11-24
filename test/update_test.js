780accc0f8af85f0f2ec0baa0d7658cb87376962
Mali Michael <malimccalla@gmail.com>
2016-11-24 09:30:16 +0000

    update tests

---
+++ test/update_test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let mali;
+
+  beforeEach((done) => {
+    mali = new User({ name: 'Mali' });
+    mali.save()
+      .then(() => {
+        done();
+      });
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Olivia');
+        done();
+      });
+  }
+
+  it('instance type using set n save', (done) => {
+    mali.set('name', 'Olivia');
+    assertName(mali.save(), done);
+  });
+
+  it('model instance can update', (done) => {
+    assertName(mali.update({ name: 'Olivia' }), done);
+  });
+
+  it('A model class can update', (done) => {
+    assertName(
+      User.update({ name: 'Mali' }, { name: 'Olivia' }),
+      done
+    );
+  });
+
+  it('A model class can update one record', (done) => {
+    assertName(
+      User.findOneAndUpdate({ name: 'Mali' }, { name: 'Olivia' }),
+      done
+    );
+  });
+
+  it('A model class can find by Id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(mali.id, { name: 'Olivia' }),
+      done
+    );
+  });
+});
