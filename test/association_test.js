d00878619fcb81894fb9f1c1185ac67024210947
Mali Michael <malimccalla@gmail.com>
2016-11-25 23:20:03 +0000

    Association tests and Promise.all

---
+++ test/association_test.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+
+describe('Associations', () => {
+  let mali;
+  let blogpost;
+  let comment;
+
+  beforeEach((done) => {
+    mali = new User({ name: 'Mali' });
+    blogpost = new BlogPost({ title: 'New Post', content: 'This is content' });
+    comment = new Comment({ content: 'This is a comment' });
+
+    mali.blogPosts.push(blogpost);
+    blogpost.comments.push(comment);
+    comment.user = mali;
+
+    Promise.all([mali.save(), blogpost.save(), comment.save()])
+      .then(() => done());
+  });
+
+  it.only('saves a realtation between a user and a blog post', (done) => {
+    User.findOne({ name: 'Mali' })
+      .then((user) => {
+        console.log(user);
+        done();
+      });
+  });
+});
