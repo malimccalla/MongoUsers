83fe8168880da72532956b442a479436c5494ef6
Mali Michael <malimccalla@gmail.com>
2016-11-28 10:37:47 +0000

    skip, limit and sort

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,11 +3,21 @@ const User = require('../src/user');
 
 describe('Reading users from database', () => {
   let mali;
+  let joe;
+  let jane;
+  let john;
 
   beforeEach((done) => {
-    mali = new User({ name: 'Mali', age: 23 });
-    mali.save()
-      .then(() => done());
+    mali = new User({ name: 'Mali' });
+    joe = new User({ name: 'Joe' });
+    jane = new User({ name: 'Jane' });
+    john = new User({ name: 'John' });
+    Promise.all([
+      mali.save(),
+      jane.save(),
+      joe.save(),
+      john.save()
+    ]).then(() => done());
   });
 
   it('finds all users with a name of Mali', (done) => {
@@ -22,6 +32,18 @@ describe('Reading users from database', () => {
     User.findOne({ _id: mali.id })
       .then((user) => {
         assert(user.name === 'Mali');
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
         done();
       });
   });
